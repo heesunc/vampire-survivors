0b31d119e565ec39833e2362a3d492b9d8ab7d15
heesunc <heesun9113@naver.com>
2022-12-26 17:43:33 +0900

    221226
    1. 플레이어 이동
    2. 새로운 인풋시스템
    3. 애니메이션 제작

--- Assets/Undead Survivor/Code/Player.cs
+++ Assets/Undead Survivor/Code/Player.cs
@@ -1,23 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
+    public float speed; // 속도를 편리하게 관리할 수 있도록 float 변수 추가
 
-    private Rigidbody2D rigid;
+    // Component 가져오기
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        // Component 가져온거 초기화 시켜주기
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
-    void Update()
+    // void Update()
+    // {
+    //     inputVec.x = Input.GetAxisRaw("Horizontal"); // 어떤 좌표 넣을 것이냐
+    //     inputVec.y = Input.GetAxis("Vertical"); 
+    // }
+
+    void FixedUpdate()
     {
-        inputVec.x = Input.GetAxis("Horizontal"); // 어떤 좌표 넣을 것이냐
-        inputVec.y = Input.GetAxis("Vertical"); 
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value) // InputValue 타입의 매개변수 작성
+    {
+        inputVec = value.Get<Vector2>(); // nomalized를 사용하고 있기 때문에 nextVec에서 지워줘도 됨
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude);
+        
+        if (inputVec.x != 0) // inputVec.x가 0이 아닐 때
+        {
+            spriter.flipX = inputVec.x < 0; // 비교 연산자의 값을 True, False로 바로 넣어줌
+        }
     }
 }
